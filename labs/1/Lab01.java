cd2c5ed41b75b778ca584b0f45ea33a33d25234c
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2019-02-08 15:13:17 -0500

    Lab1

---
+++ labs/1/Lab01.java
@@ -0,0 +1,15 @@
+
+public class Lab01 {
+
+	public static void main(String[] args) {
+		double answer1 = (15.4 * 6.5 - 4.3 * 3) / (46.3 - 3.5);
+		System.out.println("The result of (15.4 * 6.5 - 4.3 * 3) / (46.3 - 3.5) is " 
+		+ answer1);
+		
+		double Pi = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+		double ModPi =  4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1/13);
+		System.out.println("This is the result of Pi: " + Pi);
+		System.out.println("This is the result of extra Pi: " + ModPi);
+
+	}
+}
