3f48b9896c74e5ecb170e967cc38bcd114b91a7f
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2019-05-03 20:02:46 -0400

    Lab10

---
+++ labs/10/A2.java
@@ -0,0 +1,9 @@
+
+public class A2 extends AbstractClass {
+
+		void printMessage() {
+		System.out.println("This is second subclass");
+	}
+
+
+}
---
+++ labs/10/AbstractClass.java
@@ -0,0 +1,6 @@
+
+public abstract class AbstractClass {
+	abstract void printMessage();
+
+}
+
